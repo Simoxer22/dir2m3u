158ee4da5715a7ec92d2d31ed7e3683007bd055f
imDema <luca.de.martini@protonmail.com>
2019-09-06 23:53:23 +0200

    Small OsString optimization

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
 use std::error::Error;
-use std::fs::{File};
+use std::fs::File;
 use std::io::Write;
-use std::path::{PathBuf,Path};
+use std::path::{Path, PathBuf};
 
-pub struct Config
-{
-    pub directory : PathBuf,
-    pub recursive : bool,
+pub struct Config {
+    pub directory: PathBuf,
+    pub recursive: bool,
 }
 
-impl Config
-{
+impl Config {
     pub fn new(dir: &str, rec: bool) -> Result<Config, Box<dyn Error>> {
         Ok(Config {
             directory: PathBuf::from(dir),
@@ -23,8 +21,7 @@ pub fn run(conf: Config) -> Result<(), Box<dyn Error>> {
     let dest = conf.directory.canonicalize()?;
     if conf.recursive {
         recursive(&dest, conf.directory)?;
-    }
-    else{
+    } else {
         make_playlist(&dest, conf.directory)?;
     }
 
@@ -41,17 +38,13 @@ fn recursive(dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
     make_playlist(&dest, dir)
 }
 
-fn make_playlist (dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
-    if let Some(buf) = gen_playlist(&dir)?{
-
-        let mut filename = dir.canonicalize()?
-            .file_name().unwrap()
-            .to_str().unwrap()
-            .to_owned();
-        filename.push_str(".m3u");
+fn make_playlist(dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
+    if let Some(buf) = gen_playlist(&dir)? {
+        let filename = dir.canonicalize()?.file_name().unwrap().to_owned();
 
         let mut filepath = dest.to_owned();
         filepath.push(filename);
+        filepath.push(".m3u");
 
         let mut file = File::create(filepath)?;
         file.write(buf.as_bytes())?;
@@ -63,19 +56,16 @@ fn gen_playlist(dir: &Path) -> Result<Option<String>, Box<dyn Error>> {
     let mut buf = String::from("#EXTM3U\n");
 
     for song in dir.read_dir()? {
-        let path : PathBuf = song?.path();
+        let path: PathBuf = song?.path();
 
         if !path.is_dir() && path.extension().unwrap_or_default() == "mp3" {
-            let songname = path.file_stem()
-                .unwrap_or_default();
+            let songname = path.file_stem().unwrap_or_default();
             buf.push_str(format!("#EXTINF:{0:?}\n{1}\n", songname, path.display()).as_str());
         }
     }
-    
     if buf.len() > "#EXTM3U\n".len() {
         Ok(Some(buf))
-    }
-    else {
+    } else {
         Ok(None)
     }
 }
