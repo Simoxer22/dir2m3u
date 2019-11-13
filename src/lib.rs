1edaea707147a66e4cf62ef728793127f46adaad
imDema <luca.de.martini@protonmail.com>
2019-11-13 16:34:20 +0100

    Generalized gen_playlist for PathBuf Iterators Added test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::{PathBuf};
 
 pub struct Config {
     pub directory: PathBuf,
@@ -20,16 +20,22 @@ impl Config {
 pub fn run(conf: Config) -> Result<(), Box<dyn Error>> {
     let dest = conf.directory.canonicalize()?;
     if conf.recursive {
-        recursive(&dest, conf.directory)?;
+        recursive(|d| make_playlist(&dest, d), &conf.directory)?;
     } else {
-        make_playlist(&dest, conf.directory)?;
+        make_playlist(&dest, &conf.directory)?;
     }
 
     Ok(())
 }
 
-/// Traverse all directories contained in `dir` recursively and call `make_playlist` for each one
-fn recursive(dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
+/// Traverse all directories contained in `dir` recursively and call `make_playlist` on each one
+/// 
+/// # Arguments
+/// + `dest` destination path
+/// + `dir` directory to 
+/// 
+fn recursive<F> (closure: F, dir: &PathBuf) -> Result<(), Box<dyn Error>> 
+    where F : Fn(&PathBuf) -> Result<(), Box<dyn Error>> + Copy {
     let dirs = dir
         .read_dir()?
         .filter_map(|e| match e {
@@ -39,31 +45,13 @@ fn recursive(dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
         .filter(|p| p.is_dir());
 
     for dir in dirs {
-        recursive(dest, dir)?;
+        recursive(closure, &dir)?;
     }
-    make_playlist(&dest, dir)
+    (&closure)(&dir)
 }
 
 /// Call `gen_playlist` and write it to file
-fn make_playlist(dest: &PathBuf, dir: PathBuf) -> Result<(), Box<dyn Error>> {
-    if let Some(buf) = gen_playlist(&dir)? {
-        let mut filename = dir.canonicalize()?.file_name().unwrap().to_owned();
-        filename.push(".m3u");
-        let mut filepath = dest.to_owned();
-        filepath.push(filename);
-
-        let mut file = File::create(filepath)?;
-        file.write(buf.as_bytes())?;
-    }
-    Ok(())
-}
-
-const M3U_HEAD: &str = "#EXTM3U\n";
-
-/// Iterate over `mp3` and create m3u format String
-fn gen_playlist(dir: &Path) -> Result<Option<String>, Box<dyn Error>> {
-    let mut buf = String::from(M3U_HEAD);
-
+fn make_playlist(dest: &PathBuf, dir: &PathBuf) -> Result<(), Box<dyn Error>> {
     let songs = dir
         .read_dir()?
         .filter_map(|e| match e {
@@ -72,14 +60,60 @@ fn gen_playlist(dir: &Path) -> Result<Option<String>, Box<dyn Error>> {
         })
         .filter(|p| p.is_file() && p.extension().unwrap_or_default() == "mp3");
 
+    if let Some(buf) = gen_playlist(songs)? {
+        let mut filename = dir.canonicalize()?.file_name().unwrap().to_owned();
+        filename.push(".m3u");
+        let mut filepath = dest.to_owned();
+        filepath.push(filename);
+
+        let mut file = File::create(filepath)?;
+        file.write_all(buf.as_bytes())?;
+    }
+    Ok(())
+}
+
+const M3U_HEAD: &str = "#EXTM3U\n";
+
+/// Iterate over `mp3` and create m3u format String
+fn gen_playlist<I : Iterator<Item=PathBuf>>(songs: I) -> Result<Option<String>, Box<dyn Error>> {
+    let mut buf = String::from(M3U_HEAD);
+
     for song in songs {
         let songname = song.file_stem().unwrap_or_default();
         buf.push_str(format!("#EXTINF:{0:?}\n{1}\n", songname, song.display()).as_str());
     }
-
     if buf.len() > M3U_HEAD.len() {
         Ok(Some(buf))
     } else {
         Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn gen_playlist() {
+        let testpaths = vec![
+            PathBuf::from("./dir1/dir2/songname1.mp3"),
+            PathBuf::from("./dir1/dir3/songname2.mp3"),
+            PathBuf::from("./songname3.mp3"),
+            PathBuf::from("./dir1/dir3/dir2/songname4.mp3"),
+        ].into_iter();
+
+        let generated_lines = super::gen_playlist(testpaths).unwrap().unwrap();
+
+        let mut generated_iter = generated_lines.split('\n');
+
+        assert_eq!(
+            generated_iter.next(),
+            Some(M3U_HEAD.trim_end())
+        );
+
+        assert_eq!(
+            generated_iter.next().unwrap()[..8],
+            "#EXTINF:"[..8]
+        )
+
     }
 }
